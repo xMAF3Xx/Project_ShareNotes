07a9d4a8b258b1b900c955a438e3a7c53ec9faca
MAF3X <145372231+xMAF3Xx@users.noreply.github.com>
2024-01-31 17:21:44 +0100

    nuovo_upload_schermata_login
    bisogna chiedere a forna conferma per i colori e la grafica

--- login_page/script_login.js
+++ login_page/script_login.js
@@ -1,4 +1,4 @@
-/*document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var openPopupBtn = document.getElementById('openPopupBtn');
     var closePopupBtn = document.getElementById('closePopupBtn');
     var popup = document.getElementById('popup');
@@ -16,12 +16,34 @@
             popup.style.display = 'none';
         }
     });
+    
+	var openAccessBtn = document.getElementById('access');
+    var accessPopup = document.getElementById('access-popup');
 
+    openAccessBtn.addEventListener('click', function() {
+        accessPopup.style.display = 'block';
+        popup.style.display = 'none';
+        registerPopup.style.display = 'none';
+    });
+
+    var closeAccessBtn = document.getElementById('closeaccess');
+    closeAccessBtn.addEventListener('click', function() {
+        accessPopup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === accessPopup) {
+            accessPopup.style.display = 'none';
+        }
+    });
+    
     var openRegisterBtn = document.getElementById('register');
     var registerPopup = document.getElementById('register-popup');
 
     openRegisterBtn.addEventListener('click', function() {
         registerPopup.style.display = 'block';
+        popup.style.display = 'none';
+        accessPopup.style.display = 'none';
     });
 
     var closeRegisterBtn = document.getElementById('closeregister');
@@ -36,42 +58,45 @@
         }
     });
 
-    var openLoginBtn = document.getElementById('login');
-    var loginPopup = document.getElementById('login-popup');
+	var openRegisterBtn = document.getElementById('register-out');
+    var registerPopup = document.getElementById('register-popup');
 
-    openLoginBtn.addEventListener('click', function() {
-        loginPopup.style.display = 'block';
+    openRegisterBtn.addEventListener('click', function() {
+        registerPopup.style.display = 'block';
+        popup.style.display = 'none';
+        accessPopup.style.display = 'none';
     });
 
-    var closeLoginBtn = document.getElementById('closelogin');
-    closeLoginBtn.addEventListener('click', function() {
+    var closeRegisterBtn = document.getElementById('closeregister');
 
-        loginPopup.style.display = 'none';
+    closeRegisterBtn.addEventListener('click', function() {
+        registerPopup.style.display = 'none';
     });
 
     window.addEventListener('click', function(event) {
-        if (event.target === loginPopup) {
-            loginPopup.style.display = 'none';
+        if (event.target === registerPopup) {
+            registerPopup.style.display = 'none';
         }
     });
+
+	var openAccessBtn = document.getElementById('access-out');
+    var accessPopup = document.getElementById('access-popup');
+
+    openAccessBtn.addEventListener('click', function() {
+        accessPopup.style.display = 'block';
+        popup.style.display = 'none';
+        registerPopup.style.display = 'none';
+    });
+
+    var closeAccessBtn = document.getElementById('closeaccess');
+    closeAccessBtn.addEventListener('click', function() {
+        accessPopup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === accessPopup) {
+            accessPopup.style.display = 'none';
+        }
+    });
+    
 });
-*/
-function azione(variabile) { 
-    if(document.getElementById(variabile).style.display=='') { 
-    document.getElementById(variabile).style.display='none'; 
-    }else{ 
-    document.getElementById(variabile).style.display=''; 
-    } 
-}
-
-function azione2(variabile1, variabile2, variabile3) { 
-    if(document.getElementById(variabile1).style.display=="none") { 
-    document.getElementById(variabile1).style.display='';
-    document.getElementById(variabile2).style.display="none";
-    document.getElementById(variabile3).style.display="none";
-    } 
-}
-
-function togliQuesto(variabile){
-    document.getElementById(variabile).style.display='none';
-}
