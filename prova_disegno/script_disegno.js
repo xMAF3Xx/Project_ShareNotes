2c370f53271b163e3813ea6ebe0e1b23c14e226d
MAF3X <145372231+xMAF3Xx@users.noreply.github.com>
2024-02-04 10:39:51 +0100

    modifiche canvas
    aggiunta_colori_canvas_e_comando_per_mettere_a_schermo_intero_+_altro

--- prova_disegno/script_disegno.js
+++ prova_disegno/script_disegno.js
@@ -1,38 +1,56 @@
+// script_disegno.js
+
 const canvas = document.getElementById('drawingCanvas');
-    const context = canvas.getContext('2d');
-    let isDrawing = false;
+const context = canvas.getContext('2d');
+let isDrawing = false;
+let drawingColor = '#ff0000'; 
 
-    function startDrawing(e) {
-        isDrawing = true;
-        draw(e);
-    }
+function startDrawing(e) {
+    isDrawing = true;
+    draw(e);
+}
 
-    function stopDrawing() {
-        isDrawing = false;
-        context.beginPath();
-    }
+function stopDrawing() {
+    isDrawing = false;
+    context.beginPath();
+}
 
-    function draw(e) {
-        if (!isDrawing) return;
+function setDrawingColor(color) {
+    drawingColor = color;
+}
 
-        const { clientX, clientY } = e.touches ? e.touches[0] : e;
-        const x = clientX - canvas.offsetLeft;
-        const y = clientY - canvas.offsetTop;
+function draw(e) {
+    if (!isDrawing) return;
 
-        context.lineWidth = 5;
-        context.lineCap = 'round';
-        context.strokeStyle = '#000';
+    const { clientX, clientY } = e.touches ? e.touches[0] : e;
+    const x = clientX - canvas.offsetLeft;
+    const y = clientY - canvas.offsetTop;
 
-        context.lineTo(x, y);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(x, y);
-    }
+    context.lineWidth = 5;
+    context.lineCap = 'round';
+    context.strokeStyle = drawingColor; // Utilizza il colore del disegno
 
-    canvas.addEventListener('mousedown', startDrawing);
-    canvas.addEventListener('mouseup', stopDrawing);
-    canvas.addEventListener('mousemove', draw);
+    context.lineTo(x, y);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(x, y);
+}
 
-    canvas.addEventListener('touchstart', startDrawing);
-    canvas.addEventListener('touchend', stopDrawing);
-    canvas.addEventListener('touchmove', draw);
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mousemove', draw);
+
+canvas.addEventListener('touchstart', startDrawing);
+canvas.addEventListener('touchend', stopDrawing);
+canvas.addEventListener('touchmove', draw);
+
+// Aggiungi un ascoltatore per l'input del selettore di colore
+const colorPicker = document.getElementById('colorPicker');
+colorPicker.addEventListener('input', function() {
+    setDrawingColor(colorPicker.value);
+});
+
+// Imposta le dimensioni della canvas come le dimensioni della finestra
+var canvasDim = document.querySelector('canvas');
+canvasDim.width = window.innerWidth;
+canvasDim.height = window.innerHeight;
