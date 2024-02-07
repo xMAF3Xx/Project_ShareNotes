6a4e0cc269a9f6c0d90d1cf1cadd37035744578b
MAF3X <145372231+xMAF3Xx@users.noreply.github.com>
2024-02-07 16:55:51 +0100

    new_version_login_page
    fatte tutte le correzioni logiche e grafiche, per revisione finale

--- Sito_Index/script_login.js
+++ Sito_Index/script_login.js
@@ -1,103 +1,181 @@
 document.addEventListener('DOMContentLoaded', function () {
-        var popup = document.getElementById('popup');
-        var openPopupBtn = document.getElementById('openPopupBtn');
-        var closePopupBtn = document.getElementById('closePopupBtn');
-        var accessPopup = document.getElementById('access-popup');
-        var openAccessBtn = document.getElementById('access');
-        var closeAccessBtn = document.getElementById('closeaccess');
-        var openAccessOutBtn = document.getElementById('access-out');
-        var registerPopup = document.getElementById('register-popup');
-        var openRegisterBtn = document.getElementById('register');
-        var closeRegisterBtn = document.getElementById('closeregister');
-        var openRegisterOutBtn = document.getElementById('register-out');
-        function showPopup(popupElement) {
-            popupElement.style.display = 'block';
-        }
-        function hidePopup(popupElement) {
-            popupElement.style.display = 'none';
-        }
-        openPopupBtn.addEventListener('click', function () {
-            showPopup(popup);
-        });
+	var popup = document.getElementById('popup');
+    var openPopupBtn = document.getElementById('openPopupBtn');
+    var closePopupBtn = document.getElementById('closePopupBtn');
+    var accessPopup = document.getElementById('access-popup');
+    var openAccessBtn = document.getElementById('access');
+    var closeAccessBtn = document.getElementById('closeaccess');
+    var openAccessOutBtn = document.getElementById('access-out');
+    var registerPopup = document.getElementById('register-popup');
+    var openRegisterBtn = document.getElementById('register');
+    var closeRegisterBtn = document.getElementById('closeregister');
+    var openRegisterOutBtn = document.getElementById('register-out');
 
-        closePopupBtn.addEventListener('click', function () {
-            hidePopup(popup);
-        });
+	 function inizialiseAccessBtn() {
+	 		var bottone3 = document.getElementById("avanti-access");
+	 		document.getElementById("avanti-access").disabled = true;
+            bottone3.style.backgroundColor = "gray";
+            bottone3.style.borderColor = "gray";
+            bottone3.classList.add("avanti-register-button-senza-hover");
+	 }
+	 function inizialiseRegisterBtn() {
+	 		var bottone4 = document.getElementById("avanti-register");
+	 		document.getElementById("avanti-register").disabled = true;
+            bottone4.style.backgroundColor = "gray";
+            bottone4.style.borderColor = "gray";
+            bottone4.classList.add("avanti-register-button-senza-hover");
+	 }
+	 
+	 
+    function showPopup(popupElement) {
+        popupElement.style.display = 'block';
+    }
 
-        openAccessBtn.addEventListener('click', function () {
-            hidePopup(popup);
-            hidePopup(registerPopup);
-            showPopup(accessPopup);
-        });
+    function hidePopup(popupElement) {
+        popupElement.style.display = 'none';
+    }
 
-        closeAccessBtn.addEventListener('click', function () {
-            hidePopup(accessPopup);
-        });
+    openPopupBtn.addEventListener('click', function () {
+        showPopup(popup);
+    });
 
-        openAccessOutBtn.addEventListener('click', function () {
-            hidePopup(popup);
-            hidePopup(registerPopup);
-            showPopup(accessPopup);
-        });
+    closePopupBtn.addEventListener('click', function () {
+        hidePopup(popup);
+    });
 
-        openRegisterBtn.addEventListener('click', function () {
-            hidePopup(popup);
-            hidePopup(accessPopup);
-            showPopup(registerPopup);
-        });
+    openAccessBtn.addEventListener('click', function () {
+        hidePopup(popup);
+        hidePopup(registerPopup);
+        inizialiseAccessBtn();
+        showPopup(accessPopup);
+    });
 
-        closeRegisterBtn.addEventListener('click', function () {
-            hidePopup(registerPopup);
-        });
+    closeAccessBtn.addEventListener('click', function () {
+        hidePopup(accessPopup);
+    });
 
-        openRegisterOutBtn.addEventListener('click', function () {
-            hidePopup(popup);
-            hidePopup(accessPopup);
-            showPopup(registerPopup);
-        });
+    openAccessOutBtn.addEventListener('click', function () {
+        hidePopup(popup);
+        hidePopup(registerPopup);
+        inizialiseAccessBtn();
+        showPopup(accessPopup);
+    });
 
-		document.getElementById('showPasswordAccess').addEventListener('change', function() {
-      	var passwordInput = document.getElementById('passwordInputaccess');
-      	passwordInput.type = this.checked ? 'text' : 'password';
-  		});
-  		
-		document.getElementById('showPasswordRegister').addEventListener('change', function() {
-      	var passwordInput = document.getElementById('passwordInput');
-      	passwordInput.type = this.checked ? 'text' : 'password';
-  		});
-  		
-        function controllaInput() {
-            var valoreInput1 = document.getElementById('passwordInput').value;
-            var valoreInput2 = document.getElementById('passwordInputControl').value;
-            var controlloElement = document.getElementById('controllo');
-            var bottone = document.getElementById("avanti-register");
+    openRegisterBtn.addEventListener('click', function () {
+        hidePopup(popup);
+        hidePopup(accessPopup);
+        inizialiseRegisterBtn();
+        showPopup(registerPopup);
+    });
 
-            if (valoreInput1 === valoreInput2) {
-            	controlloElement.innerText = '';
-            	document.getElementById("avanti-register").disabled = false;
-            	bottone.style.backgroundColor = "#FF0000";
-            	bottone.style.borderColor = "#FF0000";
-            	bottone.classList.remove("avanti-register-button-senza-hover");
-            } else {
-                controlloElement.innerText = 'le password non corrispondono';
-                document.getElementById("avanti-register").disabled = true; 
-                bottone.style.backgroundColor = "gray";
-                bottone.style.borderColor = "gray"
-                bottone.classList.add("avanti-register-button-senza-hover");
-            }
-        }
+    closeRegisterBtn.addEventListener('click', function () {
+        hidePopup(registerPopup);
+    });
 
+    openRegisterOutBtn.addEventListener('click', function () {
+        hidePopup(popup);
+        hidePopup(accessPopup);
+        inizialiseRegisterBtn();
+        showPopup(registerPopup);
+    });
+
+    document.getElementById('showPasswordAccess').addEventListener('change', function () {
+        var passwordInputAccess = document.getElementById('passwordInputaccess');
+        passwordInputAccess.type = this.checked ? 'text' : 'password';
+    });
+
+    document.getElementById('showPasswordRegister').addEventListener('change', function () {
         var passwordInput = document.getElementById('passwordInput');
-        var passwordInputControl = document.getElementById('passwordInputControl');
+        passwordInput.type = this.checked ? 'text' : 'password';
+    });
 
-        passwordInput.addEventListener('blur', controllaInput);
-        passwordInputControl.addEventListener('blur', controllaInput);
-  	  	});
+    function controllaPassword() {
+        var valoreInput1 = document.getElementById('passwordInput').value;
+        var valoreInput2 = document.getElementById('passwordInputControl').value;
+        var controlloElement = document.getElementById('controllo');
+        var bottone = document.getElementById("avanti-register");
+        var emailInput = document.getElementById('emailInput');
+        var email = emailInput.value;
+        var nick = document.getElementById('nickinput').value;
 
-        function checkEmail(theForm) {
-            var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-]{2,})+\.)+([a-zA-Z0-9]{2,})+$/
-            if (re.test(theForm.news.value))
-                return true;
-            alert("l'eMail \"" + theForm.news.value + "\" NON \u00c8 valida!");
-            return false;
+        if (valoreInput1 === valoreInput2 && checkEmail(email) && nick !== '' && valoreInput1 !== '') {
+            controlloElement.innerText = '';
+            document.getElementById("avanti-register").disabled = false;
+            bottone.style.backgroundColor = "#FF0000";
+            bottone.style.borderColor = "#FF0000";
+            bottone.classList.remove("avanti-register-button-senza-hover");
+        } else {
+            controlloElement.innerText = 'credenziali non valide';
+            document.getElementById("avanti-register").disabled = true;
+            bottone.style.backgroundColor = "gray";
+            bottone.style.borderColor = "gray";
+            bottone.classList.add("avanti-register-button-senza-hover");
         }
+    }
+
+    var passwordInput = document.getElementById('passwordInput');
+    var passwordInputControl = document.getElementById('passwordInputControl');
+
+    passwordInput.addEventListener('change', controllaPassword);
+    passwordInputControl.addEventListener('change', controllaPassword);
+    nickinput.addEventListener('change', controllaPassword);
+
+    function checkEmail(email) {
+        var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-]{2,})+\.)+([a-zA-Z0-9]{2,})+$/;
+        return re.test(email);
+    }
+
+    function controllaEmail() {
+        var emailInput = document.getElementById('emailInput');
+        var email = emailInput.value;
+        var controlloElement = document.getElementById('controllo');
+        var bottone = document.getElementById("avanti-register");
+        var valoreInput1 = document.getElementById('passwordInput').value;
+        var valoreInput2 = document.getElementById('passwordInputControl').value;
+        var nick = document.getElementById('nickinput').value;
+
+        if (checkEmail(email) && valoreInput1 === valoreInput2 && nick !== '' && valoreInput1 !== '') {
+            controlloElement.innerText = '';
+            document.getElementById("avanti-register").disabled = false;
+            bottone.style.backgroundColor = "#FF0000";
+            bottone.style.borderColor = "#FF0000";
+            bottone.classList.remove("avanti-register-button-senza-hover");
+        } else {
+            controlloElement.innerText = 'credenziali non valide';
+            document.getElementById("avanti-register").disabled = true;
+            bottone.style.backgroundColor = "gray";
+            bottone.style.borderColor = "gray";
+            bottone.classList.add("avanti-register-button-senza-hover");
+        }
+    }
+
+    document.getElementById('emailInput').addEventListener('change', controllaEmail);
+    nickinput.addEventListener('change', controllaEmail);
+
+    function controllaLogin() {
+        var emailAccess = document.getElementById('emailInputaccess').value;
+        var passwordAccess = document.getElementById('passwordInputaccess').value;
+        var bottone2 = document.getElementById("avanti-access");
+        var controlloElement2 = document.getElementById('controlloLog');
+
+        if (checkEmail(emailAccess) && passwordAccess !== '') {
+            controlloElement2.innerText = '';
+            document.getElementById("avanti-access").disabled = false;
+            bottone2.style.backgroundColor = "#9A9CF2";
+            bottone2.style.borderColor = "#9A9CF2";
+            bottone2.classList.remove("avanti-register-button-senza-hover");
+        } else {
+            controlloElement2.innerText = 'credenziali non valide';
+            document.getElementById("avanti-access").disabled = true;
+            bottone2.style.backgroundColor = "gray";
+            bottone2.style.borderColor = "gray";
+            bottone2.classList.add("avanti-register-button-senza-hover");
+        }
+    }
+
+    document.getElementById('emailInputaccess').addEventListener('change', controllaLogin);
+    passwordInputaccess.addEventListener('change', controllaLogin);
+});
+
+
+
