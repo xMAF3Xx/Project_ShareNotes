8399c495aeccf0fc7048c236a1919dd3c4556736
MAF3X <145372231+xMAF3Xx@users.noreply.github.com>
2024-02-21 21:18:51 +0100

    profile page essentials
    caricata la pagina profilo con funzioni essenziali ad eccezione del tasto crea

---
+++ profilo/profile_page.js
@@ -0,0 +1,6 @@
+function cambiaStatoMath() {
+    var folderMath = document.getElementById("folder-math");
+    var titleSub = document.getElementById("folder-math");
+    folderMath.classList.toggle("folders-clicked"); 
+    titleSub.classList.toggle('materia-clicked');
+}
