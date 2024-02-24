4a7abd137207992c50e0b9c67dcc68ad20830a1d
MAF3X <145372231+xMAF3Xx@users.noreply.github.com>
2024-02-24 22:08:39 +0100

    new changes, and functionalities for prifle page
    sorting function

--- profilo/profile_page.js
+++ profilo/profile_page.js
@@ -4,3 +4,19 @@ function cambiaStatoMateria(id) {
     folderMath.classList.toggle("folders-clicked"); 
     titleSub.classList.toggle('materia-clicked');
 }
+
+function sort(idApp, classScomp, all) {
+	if (all === "all") {
+		var classOpen = document.getElementsByClassName(classScomp);
+		for(var i = 0; i < classOpen.length; i++) {
+    		classOpen[i].style.display = "block";
+		}
+	} else {
+		var elementiDaNascondere = document.getElementsByClassName(classScomp);
+    	var elementoDaApparire = document.getElementById(idApp);
+		for(var i = 0; i < elementiDaNascondere.length; i++) {
+    		elementiDaNascondere[i].style.display = "none";
+		}
+		elementoDaApparire.style.display = "block";
+	}
+}
