01821b347cc1cbd8bcedea0dd9f78675486e2df5
MAF3X <145372231+xMAF3Xx@users.noreply.github.com>
2024-03-19 21:27:05 +0100

    new update index page
    aggiunta di nuovi popup e perfezionamenti

--- Sito_Index/script_login.js
+++ Sito_Index/script_login.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     var popup = document.getElementById('popup');
     var openPopupBtn = document.getElementById('openPopupBtn');
     var closePopupBtn = document.getElementById('closePopupBtn');
@@ -11,10 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
     var closeRegisterBtn = document.getElementById('closeregister');
     var openRegisterOutBtn = document.getElementById('register-out');
     var blurry = document.getElementById('blurrato');
+    var closeRequirements = document.getElementById('accept-requirements');
+    var popupRequirements = document.getElementById("popup-requirements");
+    var openPopupReset = document.getElementById("open-popup-reset");
+    var popupReset = document.getElementById("popup-reset");
+    var closePopupReset = document.getElementById('close-reset');
+    var corpo = document.getElementById('corpo');
 
-    /*var popupWrapper = document.getElementById("popup-requirements");
-    popupWrapper.style.display = "block";
-    blurry.classList.add("blur-screen");*/
+    popupRequirements.style.display = "block";
+    blurry.classList.add("blur-screen");
+    corpo.classList.add("lock");
 
     function initializeAccessBtn() {
         var bottone3 = document.getElementById("avanti-access");
@@ -35,78 +41,95 @@ document.addEventListener('DOMContentLoaded', function () {
     function showPopup(popupElement) {
         popupElement.style.display = 'block';
         blurry.classList.add("blur-screen");
+        corpo.classList.add("lock");
     }
 
     function hidePopup(popupElement) {
         popupElement.style.display = 'none';
-        blurry.classList.remove("blur-screen");
+        if (popupRequirements.style.display !== 'block') {
+            blurry.classList.remove("blur-screen");
+            corpo.classList.remove("lock");
+        }
     }
 
     function closePopups() {
         hidePopup(popup);
         hidePopup(accessPopup);
         hidePopup(registerPopup);
+        hidePopup(popupReset);
     }
 
-    document.addEventListener('click', function (event) {
-        var isClickInsidePopups = popup.contains(event.target) || accessPopup.contains(event.target) || registerPopup.contains(event.target);
-    
+    document.addEventListener('click', function(event) {
+        var isClickInsidePopups = popup.contains(event.target) || accessPopup.contains(event.target) || registerPopup.contains(event.target) || popupRequirements.contains(event.target) || popupReset.contains(event.target);
         if (!isClickInsidePopups) {
             closePopups();
         }
     });
-    
-    openPopupBtn.addEventListener('click', function (event) {
-        event.stopPropagation(); 
+
+    openPopupBtn.addEventListener('click', function(event) {
+        event.stopPropagation();
         showPopup(popup);
     });
 
-    closePopupBtn.addEventListener('click', function () {
+    closePopupBtn.addEventListener('click', function() {
         hidePopup(popup);
     });
 
-    openAccessBtn.addEventListener('click', function () {
+    openPopupReset.addEventListener('click', function(event) {
+        event.stopPropagation();
+        showPopup(popupReset);
+    });
+
+    closePopupReset.addEventListener('click', function() {
+        closePopups();
+    });
+
+    closeRequirements.addEventListener('click', function() {
+        hidePopup(popupRequirements);
+    });
+
+    openAccessBtn.addEventListener('click', function() {
         hidePopup(popup);
         hidePopup(registerPopup);
         initializeAccessBtn();
         showPopup(accessPopup);
     });
 
-    closeAccessBtn.addEventListener('click', function () {
+    closeAccessBtn.addEventListener('click', function() {
         hidePopup(accessPopup);
     });
 
-    openAccessOutBtn.addEventListener('click', function () {
+    openAccessOutBtn.addEventListener('click', function() {
         hidePopup(popup);
         hidePopup(registerPopup);
         initializeAccessBtn();
         showPopup(accessPopup);
     });
 
-    openRegisterBtn.addEventListener('click', function () {
+    openRegisterBtn.addEventListener('click', function() {
         hidePopup(popup);
         hidePopup(accessPopup);
         initializeRegisterBtn();
         showPopup(registerPopup);
     });
 
-    closeRegisterBtn.addEventListener('click', function () {
+    closeRegisterBtn.addEventListener('click', function() {
         hidePopup(registerPopup);
     });
 
-    openRegisterOutBtn.addEventListener('click', function () {
+    openRegisterOutBtn.addEventListener('click', function() {
         hidePopup(popup);
         hidePopup(accessPopup);
         initializeRegisterBtn();
         showPopup(registerPopup);
     });
 
-    document.getElementById('showPasswordAccess').addEventListener('change', function () {
+    document.getElementById('showPasswordAccess').addEventListener('change', function() {
         var passwordInputAccess = document.getElementById('passwordInputaccess');
         passwordInputAccess.type = this.checked ? 'text' : 'password';
     });
 
-    document.getElementById('showPasswordRegister').addEventListener('change', function () {
+    document.getElementById('showPasswordRegister').addEventListener('change', function() {
         var passwordInput1 = document.getElementById('passwordInput');
         var passwordInput2 = document.getElementById('passwordInputControl');
         passwordInput1.type = this.checked ? 'text' : 'password';
@@ -119,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     document.getElementById('classes').addEventListener('change', validatePassword);
-    
+
     function validatePassword() {
         var passwordInput = document.getElementById('passwordInput').value;
         var confirmPasswordInput = document.getElementById('passwordInputControl').value;
@@ -173,10 +196,10 @@ document.addEventListener('DOMContentLoaded', function () {
         var password = document.getElementById('passwordInputaccess').value;
         var loginButton = document.getElementById('avanti-access');
         var controlloElement = document.getElementById('controlloLog');
-        var checkon = document.getElementById('loggedOn'); 
-        var checkoff = document.getElementById('loggedOff'); 
+        var checkon = document.getElementById('loggedOn');
+        var checkoff = document.getElementById('loggedOff');
 
-        if (checkEmail(email) && email!=="" && password !== '' && (checkon.checked || checkoff.checked)) {
+        if (checkEmail(email) && email !== "" && password !== '' && (checkon.checked || checkoff.checked)) {
             controlloElement.innerText = '';
             loginButton.disabled = false;
             loginButton.style.backgroundColor = "#9A9CF2";
@@ -190,27 +213,27 @@ document.addEventListener('DOMContentLoaded', function () {
             loginButton.classList.add("avanti-register-button-senza-hover");
         }
     }
-    
+
     loggedOn.addEventListener('change', validateLogin);
     loggedOff.addEventListener('change', validateLogin);
-    
+
     function onlyOneYes() {
-        var checkon1 = document.getElementById('loggedOn'); 
-        var checkoff1 = document.getElementById('loggedOff'); 
+        var checkon1 = document.getElementById('loggedOn');
+        var checkoff1 = document.getElementById('loggedOff');
 
         if (checkoff1.checked) {
             checkoff1.checked = false;
         }
     }
     loggedOn.addEventListener('change', onlyOneYes);
-    
+
     function onlyOneNo() {
-        var checkon1 = document.getElementById('loggedOn'); 
-        var checkoff1 = document.getElementById('loggedOff'); 
+        var checkon1 = document.getElementById('loggedOn');
+        var checkoff1 = document.getElementById('loggedOff');
 
         if (checkon1.checked) {
             checkon1.checked = false;
-        } 
+        }
     }
     loggedOff.addEventListener('change', onlyOneNo);
 
@@ -221,5 +244,24 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('passwordInputaccess').addEventListener('change', validateLogin);
     document.getElementById('emailInputaccess').addEventListener('change', validateLogin);
 
-});
 
+    function resetCtr() {
+        var emailReset = document.getElementById('emailInputReset').value;
+        var avantiResetBtn = document.getElementById('reset-password-accept');
+        var ctrReset = document.getElementById('ctr-reset');
+        if (checkEmail(emailReset)) {
+            ctrReset.innerText = '';
+            avantiResetBtn.disabled = false;
+            avantiResetBtn.style.backgroundColor = "orange";
+            avantiResetBtn.style.borderColor = "orange";
+            avantiResetBtn.classList.remove("avanti-reset-btn-senza-hover");
+        } else {
+            ctrReset.innerText = 'Credenziali non valide :(';
+            avantiResetBtn.disabled = true;
+            avantiResetBtn.style.backgroundColor = "gray";
+            avantiResetBtn.style.borderColor = "gray";
+            avantiResetBtn.classList.add("avanti-reset-btn-senza-hover");
+        }
+    }
+    document.getElementById('emailInputReset').addEventListener('change', resetCtr);
+});
