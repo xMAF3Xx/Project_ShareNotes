2c12f8f3eaf98dd6fdcdeb6d9b2208b1b01db69b
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2024-03-26 22:30:45 +0100

    1. Io non accetterò altre modifiche per questa pagina da parte di nessuno. 2. Chi osa cambiare ciò che centra con il php si scorda il 50% della parte che spetterebbe alui di vincita.

--- CERCA NOTE/cercaNote.js
+++ CERCA NOTE/cercaNote.js
@@ -8,3 +8,23 @@ function caricaContenuto() {
     var inputValue = document.getElementById('input').value;
     window.location.href = "ok.html";
 }
+
+function submitSelected(idValue, Form = 'Filters'){
+	var selectedValue = document.getElementById(idValue).value;
+    localStorage.setItem("selectedOption"+idValue, selectedValue);
+	document.getElementById(Form).submit();
+}
+
+function setSelectedValue(idValue) {
+	var selectedValue = localStorage.getItem("selectedOption"+idValue);
+	if (selectedValue) {
+		document.getElementById(idValue).value = selectedValue;
+	}
+}
+
+function setAll(){
+	setSelectedValue('classFilter');
+	setSelectedValue('annoFilter');
+    setSelectedValue('matFilter');
+	setSelectedValue('input');
+}
