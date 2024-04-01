ab9af51c23de14d9b3b92d7e6865d77919e8a470
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2024-04-01 14:32:33 +0200

    upload ultima versione pagina disegno, si chiama download....

---
+++ appunti/js/script_disegno.js
@@ -0,0 +1,24 @@
+    function Esporta(Canvas){
+        const canvasA = document.getElementById(Canvas);
+        //canvasA.getContext("2d").fillRect(10, 10, 20, 20);
+        const base64 = canvasA.toDataURL();
+        var Input = document.getElementById("salvaImmagine");
+        Input.value = base64;
+    }
+
+    function Importa(Canvas){
+        const canvasB = document.getElementById(Canvas);
+        const img = new Image();
+        img.onload = function() {
+            canvasB
+            .getContext("2d")
+            .drawImage(this, 0, 0, canvasB.width, canvasB.height);
+        };
+        const immagine = document.getElementById("contenutoNota").value;
+        img.src = immagine;
+    }
+
+    function SendIt(drawArea){
+        Esporta(drawArea);
+        document.forms["fTSend"].submit();
+    }
