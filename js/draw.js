e9215317a9a2806993108e846c215027204d7266
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2024-04-13 14:38:14 +0200

    cambiati anche tu sul pc questo cazzo di: il tuo link di destinazione con 'profile_page.php' GRAZIE

--- js/draw.js
+++ js/draw.js
@@ -1085,7 +1085,7 @@ d.addEventListener('click', function() {
 })
 
 annullaSalvataggio.addEventListener('click', function() {
-    window.location.href = 'il_tuo_link_di_destinazione';
+    window.location.href = 'profile_page.php';
 });
 
 function applicaSfocatura() {
