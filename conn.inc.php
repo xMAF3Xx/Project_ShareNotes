100796c02712c314a1839ca1b9511f31622ffd5d
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2024-05-20 09:56:12 +0200

    Update conn.inc.php

--- conn.inc.php
+++ conn.inc.php
@@ -2,8 +2,8 @@
 <html>
     <body>
         <?php
-            //$conn = new mysqli("localhost","3d3ossola","3d3ossola","3d3ossola");
-            $conn = new mysqli("localhost","root","","share-notes");
+            $conn = new mysqli("localhost","3d3ossola","3d3ossola","3d3ossola");
+            //$conn = new mysqli("localhost","root","","share-notes");
         ?>
     </body>
 </html>
